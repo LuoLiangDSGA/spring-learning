c854790a6e1c8f1b968f6833984072b86fac6ca9
Liang Lo <ll9420416@outlook.com>
2018-02-18 09:55:55 +0800

    初始化kafka

---
+++ boot-kafka/src/main/java/org/boot/kafka/bootkafka/BootKafkaApplication.java
@@ -0,0 +1,12 @@
+package org.boot.kafka.bootkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootKafkaApplication.class, args);
+	}
+}
