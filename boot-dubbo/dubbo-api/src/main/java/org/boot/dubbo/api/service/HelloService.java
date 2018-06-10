334303cffbaaf5cd6b91279bbab21356bc32d95d
Liang Lo <ll9420416@outlook.com>
2018-06-10 18:57:25 +0800

    :tada: optimize project

---
+++ boot-dubbo/dubbo-api/src/main/java/org/boot/dubbo/api/service/HelloService.java
@@ -0,0 +1,11 @@
+package org.boot.dubbo.consumer.service;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author luoliang
+ * @date 2018/6/8
+ **/
+public interface HelloService {
+    String sayHello(String name);
+}
---
+++ boot-dubbo/dubbo-api/src/test/java/org/boot/dubbo/provider/consumer/ConsumerServiceTest.java
@@ -0,0 +1,10 @@
+package org.boot.dubbo.provider.consumer;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author luoliang
+ * @date 2018/3/20
+ **/
+public class ConsumerServiceTest {
+}
