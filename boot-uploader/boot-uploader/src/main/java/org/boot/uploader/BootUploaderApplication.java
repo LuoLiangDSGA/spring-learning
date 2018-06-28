49ca22f747034191cdbf207fbed0da4992c4ffd5
FrankLo <ll9420416@outlook.com>
2018-06-28 16:59:11 +0800

    :sparkles: review code

---
+++ boot-uploader/boot-uploader/src/main/java/org/boot/uploader/BootUploaderApplication.java
@@ -0,0 +1,15 @@
+package org.boot.uploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author luoliang
+ */
+@SpringBootApplication
+public class BootUploaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootUploaderApplication.class, args);
+    }
+}
