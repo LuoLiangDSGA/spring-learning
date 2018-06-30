ec4c13355053fa8f5390e20899f71df05a40843d
FrankLo <ll9420416@outlook.com>
2018-06-30 21:29:03 +0800

    :memo：add project

---
+++ boot-security/src/main/java/org/boot/security/BootSecurityApplication.java
@@ -0,0 +1,12 @@
+package org.boot.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootSecurityApplication.class, args);
+	}
+}
