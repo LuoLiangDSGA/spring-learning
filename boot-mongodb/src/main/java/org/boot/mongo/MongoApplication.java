5a5c63d17940c6061f724b827c3de4207b5af2f5
FrankLo <ll9420416@outlook.com>
2018-10-09 22:51:53 +0800

    :fire: init project

---
+++ boot-mongodb/src/main/java/org/boot/mongo/MongoApplication.java
@@ -0,0 +1,12 @@
+package org.boot.mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoApplication.class, args);
+    }
+}
