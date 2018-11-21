68ae8a5f804826a9822f85ef13c66df49e465038
FrankLo <ll9420416@outlook.com>
2018-11-21 18:27:21 +0800

    :tada: add new project

--- boot-distributedlock/src/main/java/org/boot/distributedlock/lock/ZookeeperDistributedLock.java
+++ boot-distributedlock/src/main/java/org/boot/distributedlock/lock/ZookeeperDistributedLock.java
@@ -7,16 +7,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.Resource;
 import java.io.IOException;
+import java.util.List;
 import java.util.Objects;
+import java.util.TreeSet;
 import java.util.concurrent.CountDownLatch;
 
 /**
  * @author luoliang
  * @date 2018/11/20
  */
-@Component
 public class ZookeeperDistributedLock {
     private static final Logger LOGGER = LoggerFactory.getLogger(ZookeeperDistributedLock.class);
     /**
@@ -32,18 +32,17 @@ public class ZookeeperDistributedLock {
      */
     private CountDownLatch connectedSignal = new CountDownLatch(1);
     /**
-     * zk地址，yaml中配置
+     * 锁名称
      */
-    @Value("config.zk.address")
-    private String zkAddress;
+    private String lockName;
     /**
-     * zk超时
+     * 当前线程创建的序列Node
      */
-    @Value("config.zk.timeout")
-    private Integer timeout;
+    private ThreadLocal<String> nodeId = new ThreadLocal<>();
     private final static byte[] DATA = new byte[0];
 
-    public ZookeeperDistributedLock() {
+    public ZookeeperDistributedLock(String zkAddress, String lockName, Integer timeout) {
+        this.lockName = lockName;
         try {
             zk = new ZooKeeper(zkAddress, timeout, watchedEvent -> {
                 LOGGER.debug("receive watcher:", watchedEvent);
@@ -79,7 +78,35 @@ public class ZookeeperDistributedLock {
     }
 
     public void lock() {
-
+        String myNode = null;
+        try {
+            //临时创建子节点
+            myNode = zk.create(root + "/" + lockName, DATA, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+            LOGGER.debug(Thread.currentThread().getName() + myNode, "===========> created");
+            // 取出所有子节点
+            List<String> childrenNodes = zk.getChildren(root, false);
+            TreeSet<String> sortedNodes = new TreeSet<>();
+            // 通过TreeSet排序
+            childrenNodes.forEach(s -> sortedNodes.add(root + "/" + s));
+            String smallNode = sortedNodes.first();
+            String preNode = sortedNodes.lower(myNode);
+            if (smallNode.equals(myNode)) {
+                LOGGER.debug(Thread.currentThread().getName() + "-------" + myNode + "-------" + "get lock");
+                // 表示获得锁
+                nodeId.set(myNode);
+                return;
+            }
+            CountDownLatch latch = new CountDownLatch(1);
+            Stat stat = zk.exists(preNode, new LockWatcher(latch));
+            if (Objects.nonNull(stat)) {
+                LOGGER.debug(Thread.currentThread().getName() + "-------" + myNode + "-------" + "wait for" + root + "/" + preNode + " release lock");
+                latch.await();
+                nodeId.set(myNode);
+//                latch = null;
+            }
+        } catch (KeeperException | InterruptedException e) {
+            LOGGER.error(e.getMessage(), e);
+        }
     }
 
     public void unlock() {
