4f48220a37a042b98dee0736678ce62e1beb85d7
FrankLo <ll9420416@outlook.com>
2018-11-27 14:54:42 +0800

    :tada: add new project

---
+++ spring-scope/src/main/java/org/spring/scope/ScopeApplication.java
@@ -0,0 +1,12 @@
+package org.spring.scope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScopeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScopeApplication.class, args);
+    }
+}
---
+++ spring-scope/src/test/java/org/spring/scope/ScopeApplicationTests.java
@@ -0,0 +1,16 @@
+package org.spring.scope;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ScopeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
