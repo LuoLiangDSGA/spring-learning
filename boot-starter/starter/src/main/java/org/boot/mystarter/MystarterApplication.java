97e4fbf210f6336011f2d299f684e9f647008a54
FrankLo <ll9420416@outlook.com>
2019-02-13 18:37:42 +0800

    :memo: refactor project

---
+++ boot-starter/starter/src/main/java/org/boot/mystarter/MystarterApplication.java
@@ -0,0 +1,14 @@
+package org.boot.mystarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MystarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MystarterApplication.class, args);
+    }
+
+}
+
