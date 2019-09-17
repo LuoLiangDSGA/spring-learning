a58ace3c29e21d1c9654c687ce2c611cb231590d
FrankLo <ll9420416@outlook.com>
2019-09-17 18:41:41 +0800

    feat: add spring event project

---
+++ spring-event/src/main/java/org/spring/listener/NotifyEventListener.java
@@ -0,0 +1,18 @@
+package org.spring.listener;
+
+import org.spring.event.NotifyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author luoliang
+ * @date 2019/9/17
+ */
+@Component
+public class NotifyEventListener implements ApplicationListener<NotifyEvent> {
+
+    @Override
+    public void onApplicationEvent(NotifyEvent event) {
+        System.out.println("Received notify event - " + event.getMessage());
+    }
+}
